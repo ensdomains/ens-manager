7a62f4af5d6dcaee103154bbf5a0480965a50b94
Jeff Lau <jeff-lau@live.com>
2017-05-15 14:53:05 +0800

    Get preimage text response

--- src/api/preimage.js
+++ src/api/preimage.js
@@ -4,6 +4,8 @@ const rootUrl = 'http://preimagedb.appspot.com/keccak256'
 
 export function checkHash(hash) {
   return fetch(`${rootUrl}/${hash}`)
+    .then(res => res.text())
+    .then(res => console.log(res))
 }
 
 //checkHash('9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658')
