d6c5703c7f7a2bb7a702d405d2b0ad0a6edbe99e
Jeff Lau <jeff-lau@live.com>
2017-05-26 17:30:28 +0800

    Selected node logic

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import app from '../App'
 import { selectNode } from '../updaters/nodes'
+import classNames from 'classnames'
 
 const handleSelectNode = (event, data) => {
   selectNode(data)
   event.stopPropagation()
 }
 
-const Node = ({ data }) =>
-  <div className="node ">
+const Node = ({ data }) => {
+
+  let classes = classNames({
+    node: true,
+    selected: data.get('name') === app.db.getIn(['selectedNode', 'name'])
+  })
+
+  return <div className={classes}>
     <div onClick={(e) => handleSelectNode(e, data)} className="node-details">{data.get('name')}</div>
 
     <div className="child-nodes">
       {data.get('nodes').size > 0 ? data.get('nodes').map(node => <Node key={node.get('name')} data={node} />) : ''}
     </div>
   </div>
+}
+
 
 const Nodes = () => (
   <div className="nodes-root">
