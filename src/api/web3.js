14d4a25a7350fae10ef3a1b209eff29cd9e28b61
Jeff Lau <jeff-lau@live.com>
2017-05-29 20:48:56 +0800

    Added additional check to account for slow metamask injection

--- src/api/web3.js
+++ src/api/web3.js
@@ -4,61 +4,63 @@ import 'whatwg-fetch'
 let web3,
     provider,
     readOnly = false,
-    ready = false;
+    ready = false,
+    injected = false
 
+function setupWeb3(){
+  return new Promise(function(resolve,reject){
+    if (typeof window.web3 !== 'undefined') {
+      //Metamask or Mist
+      provider = window.web3.currentProvider
+      web3 = new Web3(window.web3.currentProvider);
+      web3.version.getNetwork(function(err, networkId){
+        ready = true
+        injected = true
+        console.log('Metamask active')
+        resolve({web3, provider, readOnly, networkId})
+      })
+    } else {
+      //Localnode
+      let url = "http://localhost:8545"
+
+      fetch(url)
+        .then(res => {
+          console.log('local node active')
+          ready = true
+
+        })
+        .catch(error => {
+          if(error.readyState === 4 && (error.status === 400 || error.status === 200)){
+            // the endpoint is active
+            console.log('Success')
+          } else {
+            //Infura
+            console.log('The endpoint is not active. Falling back to Infura readOnly mode')
+            url = 'https://ropsten.infura.io/BW6Y98TxAjFjImkmjVnG'
+            readOnly = true
+          }
+        })
+        .then(res => {
+          provider = new Web3.providers.HttpProvider(url)
+          web3 = new Web3(provider);
+          web3.version.getNetwork(function(err, networkId){
+            ready = true
+            resolve({web3, provider, readOnly, networkId})
+          })
+        })
+
+    }
+  })
+}
 
 function web3Promise(){
-  if(ready === false){
-    return setup()
+  if (ready === false || typeof window.web3 !== 'undefined' && injected === false){
+    return setupWeb3()
   } else {
     return new Promise(function(resolve,reject){
       web3.version.getNetwork(function(err, networkId){
         resolve({web3, provider, readOnly, networkId})
       })
-    })
-  }
-
-  function setup(){
-    return new Promise(function(resolve,reject){
-      if (typeof window.web3 !== 'undefined') {
-        //Metamask
-        provider = window.web3.currentProvider
-        web3 = new Web3(window.web3.currentProvider);
-        web3.version.getNetwork(function(err, networkId){
-          ready = true
-          resolve({web3, provider, readOnly, networkId})
-        })
-      } else {
-        //Localnode
-        let url = "http://localhost:8545"
-
-        fetch(url)
-          .then(res => {
-            console.log('local node active')
-            ready = true
-
-          })
-          .catch(error => {
-            if(error.readyState === 4 && (error.status === 400 || error.status === 200)){
-              // the endpoint is active
-              console.log('Success')
-            } else {
-              //Infura
-              console.log('The endpoint is not active. Falling back to Infura readOnly mode')
-              url = 'https://ropsten.infura.io/BW6Y98TxAjFjImkmjVnG'
-              readOnly = true
-            }
-          })
-          .then(res => {
-            provider = new Web3.providers.HttpProvider(url)
-            web3 = new Web3(provider);
-            web3.version.getNetwork(function(err, networkId){
-              ready = true
-              resolve({web3, provider, readOnly, networkId})
-            })
-          })
-
-      }
     })
   }
 }
