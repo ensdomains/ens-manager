d0cfa3aeb98b6732d9f41a4e24128655e134b712
Jeff Lau <jeff-lau@live.com>
2017-05-29 10:58:24 +0800

    Added a recursive fetch for subdomains

--- src/updaters/nodes.js
+++ src/updaters/nodes.js
@@ -28,6 +28,13 @@ export function updateForm(formName, data) {
 
 export function setNodeDetails(name, address) {
   //TODO: event log for subdomains
+  const recursiveFetchSubdomains = name =>
+    getSubdomains(name).then(subdomains => {
+      appendSubDomains(subdomains, name)
+      subdomains.forEach(subdomain =>
+        fetchSubdomains(subdomain.name)
+      )
+    })
 
   addNotification('Node details set')
 
@@ -36,11 +43,9 @@ export function setNodeDetails(name, address) {
       app.db.set('nodes', rootDomain)
     )
     return name
-  }).then(name =>
-    getSubdomains(name).then(subdomains =>
-      appendSubDomains(subdomains, name)
-    )
-  )
+  }).then(recursiveFetchSubdomains)
+
+
 
 
   getResolver(name).then(data =>
