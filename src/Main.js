b66a3950b5a748991bb59ff470ea72999d052067
Jeff Lau <jeff-lau@live.com>
2017-06-19 12:35:47 +0800

    Fixed hidden notifications and moved them to the right

--- src/Main.js
+++ src/Main.js
@@ -13,6 +13,22 @@ import {
 
 const Main = () => {
   console.log(db.toJS())
+  const barStyleFactory = (index, style) => ({
+   ...style,
+   ...{
+     bottom: `${2 + (index * 4)}rem`,
+     left: 'auto',
+     right: '-100%'
+   }
+  })
+  const activeBarStyleFactory = (index, style) => ({
+   ...style,
+   ...{
+     bottom: `${2 + (index * 4)}rem`,
+     left: 'auto',
+     right: '1rem'
+   }
+  })
   return <div className="App">
     <Router>
       <div>
@@ -24,6 +40,8 @@ const Main = () => {
     <NotificationStack
       notifications={db.get('notifications').toJS()}
       onDismiss={(notifcation) => removeNotification(notifcation.key)}
+      barStyleFactory={barStyleFactory}
+      activeBarStyleFactory={activeBarStyleFactory}
     />
   </div>
 }
