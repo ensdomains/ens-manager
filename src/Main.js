77b05cf433f3c59ba0436569c221b86cfa72cd47
Jeff Lau <jeff-lau@live.com>
2017-06-28 01:23:33 +0800

    New implementation of analytics

--- src/Main.js
+++ src/Main.js
@@ -4,16 +4,27 @@ import ReverseRecord from './pages/ReverseRecord'
 import Notifications from './components/Notifications'
 //import Tracker from '../components/Tracker'
 
-import { db } from 'redaxe'
+import createHistory from 'history/createBrowserHistory'
 
+import ReactGA from 'react-ga';
+
+import { db } from 'redaxe'
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
 
+ReactGA.initialize('UA-101611202-1');
+
+const history = createHistory()
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
 const Main = () => {
   return <div className="App">
-    <Router>
+    <Router history={history}>
       <div>
         <Route exact path="/" component={DomainManager}/>
         <Route path="/reverse-record" component={ReverseRecord}/>
